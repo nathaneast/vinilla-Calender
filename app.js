1a329f2138d0d1ebadedd1bf70f6f8cb28c4be20
nathan <ahrl_ahrl@naver.com>
2019-09-28 17:11:24 +0900

    4

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ var clickViewDay = document.querySelector(".clickResult-Day");
 var clickViewDate = document.querySelector(".clickResult-Date");
 
 var nowDate = new Date();
-//버튼 누름에 따라서 바뀌는 년도,날짜
+//버튼 누름에 따라서 바뀌는 년도,월
+var nowClick;
 
 function removeCalender() {
+    if (nowClick !== undefined) {
+        nowClick.removeAttribute("style");
+        nowClick = undefined;
+    }
     var allDate = date.children[0].children.length;
     for (var i = 1; i < allDate; i++) {
         for (var j = 0; j < 7; j++) {
             var removeDate = date.children[0].children[i].children[j];
             removeDate.innerText = "";
-            if (removeDate.className.length > 0) {
+            if (removeDate.className !== "") {
                 removeDate.classList.remove(removeDate.className);
             }
             removeDate.removeEventListener("click", clickDate);
@@ -30,28 +35,51 @@ function calenderHandler(event) {
     }
     nowDate = temDate;
     removeCalender();
-    viewNowCalender();
+    viewCalender();
 }
 
 function clickDate(event) {
+    if (nowClick === undefined) {
+        event.target.style.color = "white";
+        event.target.style.background = "red";
+        nowClick = event.target;
+    } else if (nowClick === event.target) {
+        nowClick.removeAttribute("style");
+        nowClick = undefined;
+    } else {
+        nowClick.removeAttribute("style");
+        event.target.style.color = "white";
+        event.target.style.background = "red";
+        nowClick = event.target;
+    }
+
     var clickDateValue = event.target.className;
     var clickDateResult = new Date(nowDate.getFullYear(), nowDate.getMonth(), clickDateValue);
+    clickView(clickDateResult);
+}
+
+function clickView(dateValue) {
     var options = {
         weekday: "long"
     };
-    clickViewDay.innerText = new Intl.DateTimeFormat('en-US', options).format(clickDateResult);
-    clickViewDate.innerText = clickDateValue;
+    clickViewDay.innerText = new Intl.DateTimeFormat('en-US', options).format(dateValue);
+    clickViewDate.innerText = dateValue.getDate();
+
+    if (dateValue.getDay() === 6) {
+        clickViewDate.style.color = "blue";
+        clickViewDay.style.color = "blue";
+
+    } else if (dateValue.getDay() === 0) {
+        clickViewDate.style.color = "red";
+        clickViewDay.style.color = "red";
+
+    } else {
+        clickViewDate.style.color = "black";
+        clickViewDay.style.color = "black";
+    }
 }
 
-function viewToday() {
-    var options = {
-        weekday: "long"
-    };
-    clickViewDay.innerText = new Intl.DateTimeFormat('en-US', options).format(nowDate);
-    clickViewDate.innerText = nowDate.getDate();
-}
-
-function viewNowCalender() {
+function viewCalender() {
     var inputWeek = 1;
     var inputDay;
     var firstDay = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1).getDay();
@@ -65,7 +93,7 @@ function viewNowCalender() {
         month: "long"
     };
 
-    nowMonthYear.innerText = new Intl.DateTimeFormat('en-US', options).format(nowDate) + " "+nowDate.getFullYear();
+    nowMonthYear.innerText = new Intl.DateTimeFormat('en-US', options).format(nowDate) + " " + nowDate.getFullYear();
 
     for (var i = 1; i <= lastDate; i++) {
         if (i === 1) {
@@ -99,8 +127,8 @@ function viewNowCalender() {
 }
 
 function init() {
-    viewNowCalender();
-    viewToday();
+    viewCalender();
+    clickView(nowDate);
     nextMonth.addEventListener("click", calenderHandler);
     backMonth.addEventListener("click", calenderHandler);
 }
